7f7cc243e0234ce219d241dd2dcf637417b167a2
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2023-10-30 15:35:50 +0800

    added part3 complete

--- lab 5/part2/scoll.v
+++ lab 5/part2/scoll.v
@@ -4,26 +4,26 @@ module scroll(
   output [15:0] display
 );
 
-	reg [3:0] count;
-    wire [3:0] a, b, c, d;
+  reg [3:0] count;
+  wire [3:0] a, b, c, d;
 
-	always @(posedge clk)
-	begin
-     if (rst)
-       count <= 4'd0;
-     else
-       count <= count + 1'b1;
-      end
+  always @(posedge clk)
+  begin
+    if (rst)
+      count <= 4'd0;
+    else
+      count <= count + 1'b1;
+  end
 
-    assign a = count;
-    assign b = count + 2'b01;
-    assign c = count + 2'b10;
-    assign d = count + 2'b11;
+  assign a = count;
+  assign b = count + 2'b01;
+  assign c = count + 2'b10;
+  assign d = count + 2'b11;
 
-      // instantiate convert module for each letter to 
-        convert bitA(.in(a), .out(display[15:12]));
-        convert bitB(.in(b), .out(display[11:8]));
-        convert bitC(.in(c), .out(display[7:4]));
-        convert bitD(.in(d), .out(display[3:0]));
+  // instantiate convert module for each letter to 
+  convert bitA(.in(a), .out(display[15:12]));
+  convert bitB(.in(b), .out(display[11:8]));
+  convert bitC(.in(c), .out(display[7:4]));
+  convert bitD(.in(d), .out(display[3:0]));
 
 endmodule
